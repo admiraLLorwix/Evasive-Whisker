3d295127c5b17d06a194fd4b6f7aae5f65c2400f
apollo-sai <earltolentino4579@gmail.com>
2026-09-17 00:03:30 +0800

    Phase 1 Done
    - Updated and Finalized Card.cs
    - Followed SO_Card_ naming conventions
    - Populated Card Assets

--- Assets/Scripts/Cards/Card.cs
+++ Assets/Scripts/Cards/Card.cs
@@ -1,21 +1,67 @@
 using UnityEngine;
 
-namespace ClowderStudio.EvasiveWhisker
+namespace EvasiveWhisker.Cards
 {
-    [CreateAssetMenu(fileName = "New Card", menuName = "Card")]
-    public class Card : ScriptableObject
+    public enum CardType
     {
-
-
-		[SerializeField] public string cardName;
-		[SerializeField] public CardType type;
-        [SerializeField] public int size = 1;
-        [SerializeField] public string effectText;
-        [SerializeField] public string flavorText;
-
-        public enum CardType { WhiskerBlower, Custody, Ability, Trap, Counterplay }
-
+        Ability,
+        Counterplay,
+        Trap,
+        WhiskerBlower,
+        Custody,
+        DetectiveCat
     }
 
-}
+    public enum TrapEffect
+    {
+        None,
+        Swipe,
+        RobinPaw,
+        Catnip,
+        CatFlash
+    }
 
+    [CreateAssetMenu(fileName = "SO_Card_NewCard", menuName = "Evasive Whisker/Card Data")]
+    public class Card : ScriptableObject
+    {
+        [Header("Card Header")]
+        public string cardID;
+        public string cardName;
+        public CardType cardType;
+
+
+
+        [Header("Card Art")]
+        public Sprite cardIllustration;
+        public Sprite cardFrame;
+
+
+
+        [Header("Card Use Case")]
+        public string cardUseCase;
+
+
+
+        [Header("Card Description")]
+        [TextArea(2, 4)]
+        public string effectDescription;
+
+
+
+        [Header("Card Rules")]
+        public bool canBeDiscarded = true;
+        public bool occupiesAbilitySlot = false;
+
+
+
+        [Header("Trap Parameters")]
+        public TrapEffect trapEffect = TrapEffect.None;
+
+
+
+        [Header("Detective Cat Trait")]
+        public bool isOddRoundOnly = false;
+        public bool isEvenRoundOnly = false;
+        public int traitCooldownRounds = 0;
+    }
+}
