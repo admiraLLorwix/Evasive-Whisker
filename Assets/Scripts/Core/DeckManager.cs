28ae32ebf8d1aade74e0369c06b7f1d1e8a719ae
apollo-sai <earltolentino4579@gmail.com>
2026-09-18 01:33:02 +0800

    Phase 2[A]
    - removed auto import

--- Assets/Scripts/Core/DeckManager.cs
+++ Assets/Scripts/Core/DeckManager.cs
@@ -2,7 +2,6 @@ using UnityEngine;
 using System.Collections.Generic;
 using EvasiveWhisker.Cards;
 
-using Unity.InferenceEngine.Tokenization.PostProcessors.Templating;
 
 namespace EvasiveWhisker.Core
 {
